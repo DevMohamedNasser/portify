456941fc3b3b47be857c819ddb728ccdb35ba17b
devMohamedNasser <m.nasser.dev@gmail.com>
2025-10-14 04:25:45 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
 import './index.css'
 import App from './App.jsx'
-// import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./test.js"
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
