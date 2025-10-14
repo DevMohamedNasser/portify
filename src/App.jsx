b9a8f7c61a85e910a9adc990620205c7b2832730
devMohamedNasser <m.nasser.dev@gmail.com>
2025-10-14 05:09:18 +0300

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,55 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import NotFound from "./components/NotFound/NotFound.jsx";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound.jsx";
 import Layout from "./components/main/Layout/Layout";
 import Conan from "./sections/Conan/Conan";
+import Nav from "./components/main/Nav/Nav.jsx";
+import Hero from "./sections/Conan/Hero.jsx";
+import About from "./sections/About/About.jsx";
+import Skills from "./sections/Skills/Skills.jsx";
+import Projects from "./sections/Projects/Projects.jsx";
+import Contact from "./sections/Contact/Contact.jsx";
+import FooterApp from "./components/main/Footer/Footer.jsx";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "",
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "",
 
-      /* اى غلط م عندى كمبرمج ف الكود*/
-      // errorElement: <NotFound />,
+  //     /* اى غلط م عندى كمبرمج ف الكود*/
+  //     errorElement: <NotFound />,
 
-      element: <Layout />,
-      children: [
-        {
-          // path: "",
-          index: true,
-          element: <Conan />,
-        },
+  //     element: <Layout />,
+  //     children: [
+  //       {
+  //         // path: "",
+  //         index: true,
+  //         element: <Conan />,
+  //       },
 
-        {
-          path: "*", // اى مسار غير اللى فوق
-          element: <NotFound />,
-        },
-      ],
-    },
-  ]);
+  //       {
+  //         path: "*", // اى مسار غير اللى فوق
+  //         element: <NotFound />,
+  //       },
+  //     ],
+  //   },
+  // ]);
 
   return (
     <>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <Nav />
+      <Hero />
+
+      <About />
+
+      <Skills />
+
+      <Projects />
+
+      <Contact />
+
+      <FooterApp />
     </>
   );
 }
