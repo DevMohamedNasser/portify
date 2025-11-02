3cef2f765456a68a2d57483d520498ba790d3954
Mohamed Nasser <m.nasser.dev@gmail.com>
2025-11-03 00:42:50 +0200

    Update CV download link and filename

--- src/sections/Conan/Hero.jsx
+++ src/sections/Conan/Hero.jsx
@@ -54,9 +54,8 @@ export default function Hero() {
                 Download CV
               </button> */}
               <a
-                href="/Mohamed Nasser (frontend developer).pdf" download
-                target="_blank"
-                rel="noopener noreferrer"
+                href="/Mohamed_Nasser_CV.pdf"
+                download="Mohamed_Nasser_CV.pdf"
                 className="cursor-pointer bg-sky-600 text-white px-5 py-2 rounded-lg font-medium transition-colors duration-300
                 hover:bg-sky-700 active:bg-sky-800
                 dark:bg-sky-700 dark:hover:bg-sky-400 dark:active:bg-sky-300 dark:active:text-gray-900
@@ -64,6 +63,7 @@ export default function Hero() {
               >
                 Download CV
               </a>
+
             </div>
           </div>
         </div>
