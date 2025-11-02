7b714533118fbf2fecedb6363adf2bd310069dd9
Mohamed Nasser <m.nasser.dev@gmail.com>
2025-11-03 00:48:39 +0200

    Fix CV download link and attributes

--- src/sections/Conan/Hero.jsx
+++ src/sections/Conan/Hero.jsx
@@ -54,8 +54,8 @@ export default function Hero() {
                 Download CV
               </button> */}
               <a
-                href="/Mohamed_Nasser_CV.pdf"
-                download="/Portify/Mohamed_Nasser_CV.pdf"
+                href="/Portify/Mohamed_Nasser_CV.pdf"
+                download="Mohamed_Nasser_CV.pdf"
                 className="cursor-pointer bg-sky-600 text-white px-5 py-2 rounded-lg font-medium transition-colors duration-300
                 hover:bg-sky-700 active:bg-sky-800
                 dark:bg-sky-700 dark:hover:bg-sky-400 dark:active:bg-sky-300 dark:active:text-gray-900
@@ -63,6 +63,7 @@ export default function Hero() {
               >
                 Download CV
               </a>
+
 
             </div>
           </div>
